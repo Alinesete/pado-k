07be014f0329072068ef4fe2f86a81b27192148b
Alinesete <klauckac@gmail.com>
2021-04-08 12:52:28 -0300

    ponto

--- angular/padok/src/app/pages/pages.module.ts
+++ angular/padok/src/app/pages/pages.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { DashboardComponent } from './dashboard/dashboard.component';
-import { TopBarComponent } from './top-bar/top-bar.component';
+import { TopBarComponent } from '../top-bar/top-bar.component';
 import { ProdutosComponent } from './produtos/produtos.component';
 
 
